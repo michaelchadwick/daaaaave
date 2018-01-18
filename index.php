0cfb42437d7eaae39e941aa1db3760cd9cb4e769
Michael Chadwick <mchadwick@ucsd.edu>
2018-01-18 12:57:04 -0800

    removed main html results and just point to api; refactored api

--- index.php
+++ index.php
@@ -1,127 +1,82 @@
 <?php
-  header("Access-Control-Allow-Origin: *");
-  if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['api'])) {
-    header('Content-type: application/json');
-
-    $data = array(
-      "dave" => "YES!",
-      "daave" => "YES!",
-      "daaave" => "YES!",
-      "daaaave" => "YES!",
-      "daaaaave" => "YES!",
-      "daaaaaave" => "YES!",
-      "daaaaaaave" => "YES!",
-    );
-
-    echo json_encode($data);
-  } else if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['slack'])) {
-    if(isset($_GET['token']) && $_GET['token'] == 'lrq432zlxosmAbfsNNhhG6yc') {
-      $songs = array(
-        "Docking",
-        "Road Trip",
-        "Charlotte Said",
-        "Wondering Why",
-        "The Ladder",
-        "Fudge and Jam",
-        "Tattoo Fetish",
-        "Pinto",
-        "Scenes From My Window",
-        "Overjoyed",
-        "Beyond Today"
-      );
-      $responses = array(
-        "Yeah, Dave is with you, man.",
-        "Damn right Dave is down.",
-        "*You got it!*",
-        "*Hell yes!*",
-        "Alive and all that jive.",
-        "Dave has got your back.",
-        "I'm on my way.",
-        "Keep *rockin'*, and Dave will be there.",
-        "Just call me _Daaaaaaave_.",
-        "I'm feeling just the right amount of *Dave* today, buddy.",
-        "You rang?",
-        "Hey, just listening to _" . $songs[rand(0,count($songs)-1)] . "_ right now. It *rules*!"
-      );
-      $json = array(
-        "response_type" => "in_channel",
-        "text" => $responses[rand(0,count($responses)-1)]
-      );
+  header('Access-Control-Allow-Origin: *');
+  // make sure it's a GET
+  if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    // plain API call
+    if(isset($_GET['api'])) {
       header('Content-type: application/json');
-      echo json_encode($json);
-    }
-  } else {
-?>
 
-<!doctype html>
-<html>
-<head>
-  <title>Is Dave Still With Us?</title>
-  <style>
-    body, html {
-      width: 100%;
-      height: 100%;
-      margin: 0 auto;
-      text-align: center;
-      font-size: 60px;
-      font-family: Impact;
-    }
-    h1 {
-      font-size: 1.5em;
-      margin: 0;
-      padding: 0;
-    }
-    a, a:active, a:visited {
-      color: blue;
-      font-size: 1em;
-    }
-    h2 {
-      color: white;
-      font-size: 4em;
-    }
-    #wrap {
-      padding: 20px;
-    }
-    #findHim {
-      text-transform: uppercase;
-    }
-    .hidden {
-      height: 10px;
-      display: block;
-      width: 100%;
-    }
-  </style>
-  <script>
-    function makeSpace(lines) {
-      var elem, elem2;
-      while(lines > 0) {
-        elem = document.createElement('div');
-        elem.classList.add('hidden');
-        document.getElementById('wrap').appendChild(elem);
-        lines--;
+      $data = array('dave', 'daave', 'daaave', 'daaaave', 'daaaaave', 'daaaaaave', 'daaaaaaave');
+
+      // random dave
+      if(isset($_GET['random'])) {
+        // random dave?
+        echo $data[rand(0, count($data) - 1)];
+      }
+      // all dave
+      else {
+        echo json_encode($data);
       }
     }
-  </script>
-</head>
-<body>
-  <div id="wrap">
-    <h1>Is <strong style='color: red'>Dave</strong> Still With Us?</h1>
-    <p><a href="#" id="findHim">Click here to find out!</a></p>
-    <script>makeSpace(500);</script>
-    <a name="daaaaave"></a>
-    <script>makeSpace(30);</script>
-    <h2>YES!</h2>
-    <script>makeSpace(30);</script>
-  </div>
-  <script>
-    document.getElementById('findHim').addEventListener('click', function(e) {
-      e.preventDefault();
-      document.body.style.backgroundColor = "purple";
-      document.location = "/#daaaaave";
-    });
-  </script>
-</body>
-</html>
-<?php
-}
+    // slack api call
+    else if(isset($_GET['slack'])) {
+      // got token
+      if(isset($_GET['token']) && $_GET['token'] == 'lrq432zlxosmAbfsNNhhG6yc') {
+        $songs = array(
+          'Docking',
+          'Road Trip',
+          'Charlotte Said',
+          'Wondering Why',
+          'The Ladder',
+          'Fudge and Jam',
+          'Tattoo Fetish',
+          'Pinto',
+          'Scenes From My Window',
+          'Overjoyed',
+          'Beyond Today',
+          'Even Tide',
+          'Presto Change-o',
+          'SoCal Switchblade',
+          'Unwound String',
+          'Slow Boat',
+          'Down and Out',
+          'Humanity'
+        );
+        $responses = array(
+          'Uh, that\'s *David* to you. Heh. Just kidding! Dave is fine.',
+          'Dave\'s not here right now.',
+          'You called?',
+          'Yeah, Dave is with you, man.',
+          'Damn right Dave is down.',
+          '*You got it!*',
+          '*Hell yes!*',
+          '*Woo!*',
+          'Dave is caught in an interdimensional rock warp, but will be back soon.',
+          'Alive and all that jive.',
+          'Just reeling from how badass ya\'ll are.',
+          'Dave has got your back.',
+          'I\'m on my way.',
+          'Keep *rockin\'*, and Dave will be there.',
+          'Just call me _Daaaaaaave_.',
+          'I\'m feeling just the right amount of *Dave* today, buddy.',
+          'You rang?',
+          'Hey, just listening to _' . $songs[rand(0,count($songs)-1)] . '_ right now. It *rules*!'
+        );
+        $json = array(
+          'response_type' => 'in_channel',
+          'text' => $responses[rand(0,count($responses)-1)]
+        );
+        header('Content-type: application/json');
+        echo json_encode($json);
+      }
+      // no token
+      else {
+        echo '<p>API token missing</p>';
+      }
+    }
+    // no api or slack, so html
+    else {
+      echo '<p>Try the <a href="/?api=1">API</a> instead</p>';
+    }
+  }
 ?>
