d8c8fb9358cc32d68e434e70043dd507c7b627be
Michael Chadwick <mchadwick@ucsd.edu>
2018-01-19 09:49:32 -0800

    more api logic; refactored dave api

--- index.php
+++ index.php
@@ -6,77 +6,87 @@
     if(isset($_GET['api'])) {
       header('Content-type: application/json');
 
-      $data = array('dave', 'daave', 'daaave', 'daaaave', 'daaaaave', 'daaaaaave', 'daaaaaaave');
+      $dave = [];
+      for($i = 0; $i < 40; $i++) {
+        $dave[$i] = 'd' . (str_repeat('a', $i + 1)) . 've';
+      }
 
       // random dave
       if(isset($_GET['random'])) {
-        // random dave?
-        echo $data[rand(0, count($data) - 1)];
+        $index = rand(0, count($dave) - 1);
+        echo $dave[$index];
       }
       // all dave
       else {
-        echo json_encode($data);
+        echo json_encode($dave);
       }
     }
     // slack api call
     else if(isset($_GET['slack'])) {
       // got token
-      if(isset($_GET['token']) && $_GET['token'] == $_ENV['DAVE_SLACK_TOKEN']) {
-        $songs = array(
-          'Docking',
-          'Road Trip',
-          'Charlotte Said',
-          'Wondering Why',
-          'The Ladder',
-          'Fudge and Jam',
-          'Tattoo Fetish',
-          'Pinto',
-          'Scenes From My Window',
-          'Overjoyed',
-          'Beyond Today',
-          'Even Tide',
-          'Presto Change-o',
-          'SoCal Switchblade',
-          'Unwound String',
-          'Slow Boat',
-          'Down and Out',
-          'Humanity'
-        );
-        $responses = array(
-          'Uh, that\'s *David* to you. Heh. Just kidding! Dave is fine.',
-          'Dave\'s not here right now.',
-          'You called?',
-          'Yeah, Dave is with you, man.',
-          'Damn right Dave is down.',
-          '*You got it!*',
-          '*Hell yes!*',
-          '*Woo!*',
-          'Dave is caught in an interdimensional rock warp, but will be back soon.',
-          'Alive and all that jive.',
-          'Just reeling from how badass ya\'ll are.',
-          'Dave has got your back.',
-          'I\'m on my way.',
-          'Keep *rockin\'*, and Dave will be there.',
-          'Just call me _Daaaaaaave_.',
-          'I\'m feeling just the right amount of *Dave* today, buddy.',
-          'You rang?',
-          'Hey, just listening to _' . $songs[rand(0,count($songs)-1)] . '_ right now. It *rules*!'
-        );
-        $json = array(
-          'response_type' => 'in_channel',
-          'text' => $responses[rand(0,count($responses)-1)]
-        );
-        header('Content-type: application/json');
-        echo json_encode($json);
+      if(isset($_GET['token'])) {
+        // token matches
+        if($_GET['token'] == $_ENV["DAVE_SLACK_TOKEN"]) {
+          $songs = array(
+            'Docking',
+            'Road Trip',
+            'Charlotte Said',
+            'Wondering Why',
+            'The Ladder',
+            'Fudge and Jam',
+            'Tattoo Fetish',
+            'Pinto',
+            'Scenes From My Window',
+            'Overjoyed',
+            'Beyond Today',
+            'Even Tide',
+            'Presto Change-o',
+            'SoCal Switchblade',
+            'Unwound String',
+            'Slow Boat',
+            'Down and Out',
+            'Humanity'
+          );
+          $responses = array(
+            'Uh, that\'s *David* to you. Heh. Just kidding! Dave is fine.',
+            'Dave\'s not here right now.',
+            'You called?',
+            'Yeah, Dave is with you, man.',
+            'Damn right Dave is down.',
+            '*You got it!*',
+            '*Hell yes!*',
+            '*Woo!*',
+            'Dave is caught in an interdimensional rock warp, but will be back soon.',
+            'Alive and all that jive.',
+            'Just reeling from how badass ya\'ll are.',
+            'Dave has got your back.',
+            'I\'m on my way.',
+            'Keep *rockin\'*, and Dave will be there.',
+            'Just call me _Daaaaaaave_.',
+            'I\'m feeling just the right amount of *Dave* today, buddy.',
+            'You rang?',
+            'Hey, just listening to _' . $songs[rand(0,count($songs)-1)] . '_ right now. It *rules*!'
+          );
+          $json = array(
+            'response_type' => 'in_channel',
+            'text' => $responses[rand(0,count($responses)-1)]
+          );
+          header('Content-type: application/json');
+          echo json_encode($json);
+        }
+        // token does not match
+        else {
+          echo 'Dave says: "What? I didn\'t understand that, dude."';
+        }
       }
       // no token
       else {
-        echo '<p>API token missing</p>';
+        echo 'Dave says: "Eh? I don\'t think I know you, guy."';
       }
     }
     // no api or slack, so html
     else {
-      echo '<p>Try the <a href="/?api=1">API</a> instead</p>';
+      echo '<p>Try the <a href="/?api=1">API</a> instead.</p>';
     }
   }
 ?>
