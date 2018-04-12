a17c97f666a79470e5981da1886f51a4bee65cd1
Michael Chadwick <mchadwick@ucsd.edu>
2018-04-12 14:19:45 -0700

    fixed some more api bugs; moved variable responses to new file

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
   require __DIR__ . '/vendor/autoload.php';
+  require __DIR__ . '/lib/variables.php';
 
   $DAVE_LIMIT_COUNT_DEFAULT = 10;
 
@@ -34,7 +35,9 @@
       $random = isset($_GET['random']);
       if(isset($random) && $random > 0) {
         $index = rand(0, count($daveArray) - 1);
-        $json = $daveArray[$index];
+        $json = array(
+          "text" => $daveArray[$index]
+        );
       }
       // otherwise, return all the daves
       else {
@@ -46,79 +49,39 @@
       $tokenExt = $_GET['token'];
 
       // did we get a token?
-      if(isset($token)) {
+      if(isset($tokenExt)) {
         // token matches
         if($tokenExt == $tokenInt) {
-          $songs = array(
-            'Docking',
-            'Road Trip',
-            'Charlotte Said',
-            'Wondering Why',
-            'The Ladder',
-            'Fudge and Jam',
-            'Tattoo Fetish',
-            'Pinto',
-            'Scenes From My Window',
-            'Overjoyed',
-            'Beyond Today',
-            'Even Tide',
-            'Presto Change-o',
-            'SoCal Switchblade',
-            'Unwound String',
-            'Slow Boat',
-            'Down and Out',
-            'Humanity'
-          );
-          $responses = array(
-            'Uh, that\'s *David* to you. Heh. Just kidding! Dave is fine.',
-            'Dave\'s not here right now.',
-            'You called?',
-            'Yeah, Dave is with you, man.',
-            'Damn right Dave is down.',
-            '*You got it!*',
-            '*Hell yes!*',
-            '*Woo!*',
-            'Dave is caught in an interdimensional rock warp, but will be back soon.',
-            'Alive and all that jive.',
-            'Just reeling from how badass ya\'ll are.',
-            'Dave has got your back.',
-            'I\'m on my way.',
-            'Keep *rockin\'*, and Dave will be there.',
-            'Just call me _Daaaaaaave_.',
-            'I\'m feeling just the right amount of *Dave* today, buddy.',
-            'You rang?',
-            'Hey, just listening to _' . $songs[rand(0,count($songs)-1)] . '_ right now. It *rules*!'
-          );
           $json = array(
-            'response_type' => 'in_channel',
-            'text' => $responses[rand(0, count($responses) - 1)]
+            "response_type" => "in_channel",
+            "text" => $responses[rand(0, count($responses) - 1)]
           );
         }
         // invalid token
         else {
           $json = array(
-            "message" => "Dave says: 'What? I didn't understand that, dude.'"
+            "text" => "Dave says: 'What? I didn't understand that, dude.'"
           );
         }
       }
       // missing token
       else {
         $json = array(
-          "message" => "Dave says: 'Eh? I don't think I know you, buddy.'"
+          "text" => "Dave says: 'Eh? I don't think I know you, buddy.'"
         );
       }
     }
     // regular http request; needs to be api
     else {
       $json = array(
-        "message" => "Dave says: 'Try the API instead, guy.'",
+        "text" => "Dave says: 'Try the API instead, guy.'",
         "link" => "?api=1"
       );
     }
   } else {
     // some other HTTP request
     $json = array(
-      "message" => "Dave says: 'I don't respond to that kind of talk, man.'"
+      "text" => "Dave says: 'I don't respond to that kind of talk, man.'"
     );
   }
 
---
+++ lib/variables.php
@@ -0,0 +1,42 @@
+<?php
+$songs = array(
+  'Docking',
+  'Road Trip',
+  'Charlotte Said',
+  'Wondering Why',
+  'The Ladder',
+  'Fudge and Jam',
+  'Tattoo Fetish',
+  'Pinto',
+  'Scenes From My Window',
+  'Overjoyed',
+  'Beyond Today',
+  'Even Tide',
+  'Presto Change-o',
+  'SoCal Switchblade',
+  'Unwound String',
+  'Slow Boat',
+  'Down and Out',
+  'Humanity'
+);
+$responses = array(
+  'Uh, that\'s *David* to you. Heh. Just kidding! Dave is fine.',
+  'Dave\'s not here right now.',
+  'You called?',
+  'Yeah, Dave is with you, man.',
+  'Damn right Dave is down.',
+  '*You got it!*',
+  '*Hell yes!*',
+  '*Woo!*',
+  'Dave is caught in an interdimensional rock warp, but will be back soon.',
+  'Alive and all that jive.',
+  'Just reeling from how badass ya\'ll are.',
+  'Dave has got your back.',
+  'I\'m on my way.',
+  'Keep *rockin\'*, and Dave will be there.',
+  'Just call me _Daaaaaaave_.',
+  'I\'m feeling just the right amount of *Dave* today, buddy.',
+  'You rang?',
+  'Hey, just listening to _' . $songs[rand(0,count($songs)-1)] . '_ right now. It *rules*!'
+);
+?>
