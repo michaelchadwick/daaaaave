5835f73b89d8975596a6e632f95b223ac646e4f1
Michael Chadwick <mchadwick@ucsd.edu>
2019-09-09 16:16:36 -0700

    allow OPTIONS

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
   header('Access-Control-Max-Age: 3600');
   header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
-  header('Access-Control-Allow-Methods: GET');
+  header('Access-Control-Allow-Methods: GET, OPTIONS');
   header('Access-Control-Allow-Origin: *');
   header('Cache-Control: must-revalidate');
   header('Content-type: application/json; charset=UTF-8');
