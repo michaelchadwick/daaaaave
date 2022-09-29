91192965385eca04c393ff93b3ec3dbfc759e521
Michael Chadwick <michael.chadwick@gmail.com>
2022-09-29 16:32:17 -0700

    added rand_name ruby command and library

---
+++ public/assets/scripts/rand_name.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# rand_name
+# returns (a) random name(s) using the name generator from gemwarrior
+# (and copies to clipboard)
+
+require_relative "lib/name_generator"
+
+reps = ARGV[0] ? ARGV[0].to_i : 1
+names = []
+
+1.upto(reps) do
+  name = NameGenerator.new('name', 'fantasy').generate_name
+  names << name
+  system("echo #{name}")
+end
+
+def pbcopy(arg)
+  IO.popen('pbcopy', 'w') { |io| io.puts arg }
+end
+
+pbcopy names
