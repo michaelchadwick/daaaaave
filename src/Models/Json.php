9cfe3e2b82a8469cfdd6052deae70d452e5ac612
Michael Chadwick <michael.chadwick@gmail.com>
2024-05-05 17:48:49 -0700

    TODO.md -> code

--- src/Models/Json.php
+++ src/Models/Json.php
@@ -4,6 +4,7 @@ namespace Dave\Models;
 
 class Json {
   public function __construct($size) {
+    // TODO: can't get more than 599 JSON items ([related bug](https://github.com/jpmens/jo/issues/132))
     $FILE_JSN_DEF_SIZE = 1;
     $FILE_JSN_MAX_SIZE = 100;
     $bypass = isset($_GET['bypass']);
